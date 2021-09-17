88c27a553add13740306fb4558f0e11c919f7091
captproton <carl@wdwhub.net>
2021-09-17 13:54:01 -0700

    ADDED: index for backstage

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ authenticate :user, lambda { |u| u.admin? } do
 end
 
   resources :notifications, only: [:index]
+  resources :backstage, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: 'home#index'
